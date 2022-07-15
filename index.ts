20a350aa76b1a232db6b07c26477df8788b40ef6
tjmuscles <tjmuscles@yahoo.com>
2022-07-15 15:05:48 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,23 @@
+import { fromEvent, Subject } from "rxjs";
+import { map } from 'rxjs/operators';
+
 const emitButton = document.querySelector('button#emit');
 const inputElement: HTMLInputElement = document.querySelector('#value-input');
 const subscribeButton = document.querySelector('button#subscribe');
 
+const value$ = new Subject<string>();
+
+fromEvent(emitButton,'click')
+.pipe(
+  map(() => inputElement.value)
+)
+.subscribe(
+  value$
+);
+
+fromEvent(subscribeButton, 'click').subscribe(
+  () => {
+    console.log('New Subscription');
+    value$.subscribe(value => console.log(value));
+  }
+);
