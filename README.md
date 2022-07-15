606a38495d17758c577c7dbf817671613ea7ecf0
tjmuscles <tjmuscles@yahoo.com>
2022-07-15 14:46:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-7d-subject-w3bsfu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-7d-subject-w3bsfu)
---
+++ index.ts
@@ -0,0 +1,4 @@
+const emitButton = document.querySelector('button#emit');
+const inputElement: HTMLInputElement = document.querySelector('#value-input');
+const subscribeButton = document.querySelector('button#subscribe');
+
